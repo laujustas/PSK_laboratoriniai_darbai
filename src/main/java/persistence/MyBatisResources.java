fa7989eafced4f80d85a5580ff4f18a33adedb5c
Justas Laurinavičius <laujustas708@gmail.com>
2023-04-05 15:23:46 +0300

    Configured MyBatis and generated files. Added relation between ticket and flight. MyBatis currently not working when trying to Inject (TicketsMyBatis.java)

---
+++ src/main/java/persistence/MyBatisResources.java
@@ -0,0 +1,27 @@
+package persistence;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.io.Resources;
+import org.mybatis.cdi.SessionFactoryProvider;
+
+import java.io.IOException;
+
+@ApplicationScoped
+public class MyBatisResources {
+
+    @Produces
+    @ApplicationScoped
+    @SessionFactoryProvider
+    private SqlSessionFactory produceSqlSessionFactory() {
+        try {
+            return new SqlSessionFactoryBuilder().build(
+                    Resources.getResourceAsStream("MyBatisConfig.xml")
+            );
+        } catch (IOException e) {
+            throw new RuntimeException("MyBatisResources.produceSqlSessionFactory(): ", e);
+        }
+    }
+}
---
+++ src/main/java/usecases/mybatis/TicketsMybatis.java
@@ -0,0 +1,40 @@
+package usecases.mybatis;
+
+import jakarta.annotation.PostConstruct;
+
+import jakarta.enterprise.inject.Model;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+import mybatis.dao.TicketMapper;
+import mybatis.model.Ticket;
+
+import java.io.Serializable;
+import java.util.List;
+
+//@ViewScoped
+//@Named
+//@Getter @Setter
+@Model
+public class TicketsMybatis{
+
+    //@Inject
+    // Commented out for now because does not want to work. Error when trying to inject:
+    // WELD-001408: Unsatisfied dependencies for type TicketMapper with qualifiers @Default
+    // at injection point [BackedAnnotatedField] @Inject private usecases.mybatis.TicketsMybatis.ticketMapper
+    private TicketMapper ticketMapper;
+
+    @Getter @Setter
+    private List<Ticket> allTickets;
+
+    public void loadTickets() {
+        this.allTickets = ticketMapper.selectAll();
+    }
+
+    @PostConstruct
+    public void init(){
+        this.loadTickets();
+    }
+}
